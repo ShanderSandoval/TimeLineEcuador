6934c1e8524f93a9a333c56c334e932bf4eaf1da
shandersandoval <sesandoval@uce.edu.ec>
2026-02-11 12:45:29 -0500

    Cambios Pagina

--- script.js
+++ script.js
@@ -6,49 +6,49 @@
       activeClass: "timeline-item--active",
       img: ".timeline__img"
     };
+
     selectors.item.eq(0).addClass(selectors.activeClass);
-    selectors.id.css(
-      "background-image",
-      "url(" +
-        selectors.item
-          .first()
-          .find(selectors.img)
-          .attr("src") +
-        ")"
-    );
-    var itemLength = selectors.item.length;
+    var firstImg = selectors.item.first().find(selectors.img).attr("src");
+    selectors.id.css("background-image", "url(" + firstImg + ")");
+
     $(window).scroll(function() {
-      var max, min;
-      var pos = $(this).scrollTop();
-      selectors.item.each(function(i) {
-        min = $(this).offset().top;
-        max = $(this).height() + $(this).offset().top;
-        var that = $(this);
-        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-          selectors.item.removeClass(selectors.activeClass);
-          selectors.id.css(
-            "background-image",
-            "url(" +
-              selectors.item
-                .last()
-                .find(selectors.img)
-                .attr("src") +
-              ")"
-          );
-          selectors.item.last().addClass(selectors.activeClass);
-        } else if (pos <= max - 40 && pos >= min) {
-          selectors.id.css(
-            "background-image",
-            "url(" +
-              $(this)
-                .find(selectors.img)
-                .attr("src") +
-              ")"
-          );
+      var scrollPos = $(this).scrollTop();
+      var windowHeight = $(window).height();
+      var centerScreen = scrollPos + (windowHeight / 2);
+
+      selectors.item.each(function() {
+        var itemOffset = $(this).offset().top;
+        var itemHeight = $(this).outerHeight();
+
+        if (centerScreen > itemOffset && centerScreen < (itemOffset + itemHeight)) {
           selectors.item.removeClass(selectors.activeClass);
           $(this).addClass(selectors.activeClass);
+          var currentImg = $(this).find(selectors.img).attr("src");
+          selectors.id.css("background-image", "url(" + currentImg + ")");
         }
       });
+    });
+
+    $(document).keydown(function(e) {
+      var activeItem = $("." + selectors.activeClass);
+      var targetItem;
+
+      if (e.which == 40) { targetItem = activeItem.next(".timeline-item"); }
+      else if (e.which == 38) { targetItem = activeItem.prev(".timeline-item"); }
+
+      if (targetItem && targetItem.length) {
+        var windowHeight = $(window).height();
+        var contentBlock = targetItem.find(".timeline__content");
+        
+        // CÁLCULO DE CENTRADO: Posición del bloque - (mitad de pantalla - mitad del bloque)
+        var targetScroll = contentBlock.offset().top - (windowHeight / 2) + (contentBlock.outerHeight() / 2);
+
+        $('html, body').stop().animate({
+          scrollTop: targetScroll
+        }, 600);
+        
+        e.preventDefault();
+      }
     });
   };
 })(jQuery);
